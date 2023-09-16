acf760966ccd6721b0acaaa3f8fd56ca36f4c99e
Vishal <105046762+vishal-kun@users.noreply.github.com>
2023-09-16 06:57:33 +0000

    conversion of dataType

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+/*
+let score = "vishal"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+*/
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1; false =>0;
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => True; 0=> false
+// "" => false
+// "abc" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
