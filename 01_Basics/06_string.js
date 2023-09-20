d255a5b5d062ee38de9c37fc4e24ec835c79a307
Vishal <105046762+vishal-kun@users.noreply.github.com>
2023-09-20 17:33:08 +0000

    strings

---
+++ 01_Basics/06_string.js
@@ -0,0 +1,29 @@
+const name = "vishal"
+const repoCount = 50
+
+// console.log(name + repoCount )
+
+
+// String Interpolation.
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('vishalky')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+
+// console.log(gameName.indexOf('l'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
+
